178f2062199ed5362e661d40d2cafd0c3f4c11c7
Carl-Emil Kjellstrand <carl-emil.kjellstrand@sonymobile.com>
2015-03-05 10:37:09 +0100

    refactoring for unit testing

--- app/src/main/java/se/kjellstrand/fiveinarow/game/RandomSearchPlayer.java
+++ app/src/main/java/se/kjellstrand/fiveinarow/game/RandomSearchPlayer.java
@@ -33,41 +33,55 @@ public class RandomSearchPlayer extends AbstractPlayer {
 
         Log.d(TAG, "+++current player " + currentPlayer.getPlayerNumber());
         for (int i = 0; i < 1; i++) {
-            FiveInARowBoard cloneBoard = new FiveInARowBoard(board, rp1, rp2);
-            Move move = rp.getNextMove(cloneBoard);
+            FiveInARowBoard cloneBoard;
+            Move move;
+
+            cloneBoard = new FiveInARowBoard(board, rp1, rp2);
+
+            move = rp.getNextMove(cloneBoard);
             Log.d(TAG, "move " + move);
+
             cloneBoard.makeMove(move, currentPlayer);
-            FiveInARow fir = null;
-            try {
-                fir = new FiveInARow(cloneBoard);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            Log.d(TAG, "before win");
-            cloneBoard.print();
-            AbstractPlayer winner = fir.playTheGame();
-            Log.d(TAG, "after win");
-            cloneBoard.print();
-            Log.d(TAG, "current player " + currentPlayer.getPlayerNumber());
-            Log.d(TAG, "move " + move + ", winner " + winner.getPlayerNumber());
-            if (winner.getPlayerNumber() == currentPlayer.getPlayerNumber()) {
-                results[move.x][move.y]++;
-            } else {
-                results[move.x][move.y]--;
-            }
+
+            AbstractPlayer winner = playRandomGameOnBoard(currentPlayer, cloneBoard, move);
+
+            updateResultBoard(results, currentPlayer, move, winner);
         }
 
-        StringBuffer sb = new StringBuffer();
-        for (int y = 0; y < board.getHeight(); y++) {
-            for (int x = 0; x < board.getWidth(); x++) {
-                sb.append(results[x][y] + ", ");
-            }
-            sb.append("\n");
-        }
-        Log.d(TAG, "----result-----\n" + sb.toString());
+        printResultArray(board, results);
+        Move bestMove = getBestMove(board, results);
+        Log.d(TAG, "best move: " + bestMove);
 
+        return bestMove;
+    }
+
+    private void updateResultBoard(int[][] results, AbstractPlayer currentPlayer, Move move, AbstractPlayer winner) {
+        if (winner.getPlayerNumber() == currentPlayer.getPlayerNumber()) {
+            results[move.x][move.y]++;
+        } else {
+            results[move.x][move.y]--;
+        }
+    }
+
+    private AbstractPlayer playRandomGameOnBoard(AbstractPlayer currentPlayer, FiveInARowBoard cloneBoard, Move move) {
+        FiveInARow fir = null;
+        try {
+            fir = new FiveInARow(cloneBoard);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Log.d(TAG, "before win");
+        cloneBoard.print();
+        AbstractPlayer winner = fir.playTheGame();
+        Log.d(TAG, "after win");
+        cloneBoard.print();
+        Log.d(TAG, "current player " + currentPlayer.getPlayerNumber());
+        Log.d(TAG, "move " + move + ", winner " + winner.getPlayerNumber());
+        return winner;
+    }
+
+    private Move getBestMove(FiveInARowBoard board, int[][] results) {
         Move bestMove = new Move(0, 0);
-
         for (int y = 0; y < board.getHeight(); y++) {
             for (int x = 0; x < board.getWidth(); x++) {
                 if (results[bestMove.x][bestMove.y] < results[x][y]) {
@@ -76,8 +90,17 @@ public class RandomSearchPlayer extends AbstractPlayer {
                 }
             }
         }
-        Log.d(TAG, "best move: " + bestMove);
-
         return bestMove;
+    }
+
+    private void printResultArray(FiveInARowBoard board, int[][] results) {
+        StringBuffer sb = new StringBuffer();
+        for (int y = 0; y < board.getHeight(); y++) {
+            for (int x = 0; x < board.getWidth(); x++) {
+                sb.append(results[x][y] + ", ");
+            }
+            sb.append("\n");
+        }
+        Log.d(TAG, "----result-----\n" + sb.toString());
     }
 }
